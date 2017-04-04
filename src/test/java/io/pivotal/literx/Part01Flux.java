bd08c914539ace812b653427db15c1e2735dc51d
Joel Crosswhite <joel.crosswhite@ix.netcom.com>
2017-04-04 10:36:08 -0600

    Completed first set of tests.

--- src/test/java/io/pivotal/literx/Part01Flux.java
+++ src/test/java/io/pivotal/literx/Part01Flux.java
@@ -26,9 +26,9 @@ public class Part01Flux {
                 .verifyComplete();
     }
 
-    // TODO Return an empty Flux
+    // Return an empty Flux
     Flux<String> emptyFlux() {
-        return null;
+        return Flux.empty();
     }
 
 //========================================================================================
@@ -40,9 +40,9 @@ public class Part01Flux {
                 .verifyComplete();
     }
 
-    // TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+    // Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
     Flux<String> fooBarFluxFromValues() {
-        return null;
+        return Flux.just("foo", "bar");
     }
 
 //========================================================================================
@@ -54,9 +54,9 @@ public class Part01Flux {
                 .verifyComplete();
     }
 
-    // TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+    // Create a Flux from a List that contains 2 values "foo" and "bar"
     Flux<String> fooBarFluxFromList() {
-        return null;
+        return Flux.fromIterable(Arrays.asList("foo", "bar"));
     }
 
 //========================================================================================
@@ -66,10 +66,10 @@ public class Part01Flux {
         StepVerifier.create(flux)
                 .verifyError(IllegalStateException.class);
     }
-    // TODO Create a Flux that emits an IllegalStateException
 
+    // Create a Flux that emits an IllegalStateException
     Flux<String> errorFlux() {
-        return null;
+        return Flux.error(new IllegalStateException());
     }
 
 //========================================================================================
@@ -81,9 +81,9 @@ public class Part01Flux {
                 .verifyComplete();
     }
 
-    // TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+    // Create a Flux that emits increasing values from 0 to 9 each 100ms
     Flux<Long> counter() {
-        return null;
+        return Flux.just(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L).delay(Duration.ofMillis(100L));
     }
 
 }
